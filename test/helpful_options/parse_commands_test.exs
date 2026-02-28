9c1d85dc32993235f3a1a22de1db48c3151625a5
Joe Yates <joe.g.yates@gmail.com>
2026-02-28 10:31:16 +0100

    Remove duplicate tests, preferring doctests

--- test/helpful_options/parse_commands_test.exs
+++ test/helpful_options/parse_commands_test.exs
@@ -2,35 +2,6 @@ defmodule HelpfulOptions.ParseCommandsTest do
   use ExUnit.Case, async: true
 
   describe "parse_commands/2" do
-    test "most specific (longest) command match wins" do
-      definitions = [
-        %{commands: ["remote"], switches: nil, other: nil},
-        %{commands: ["remote", "add"], switches: [name: %{type: :string}], other: nil}
-      ]
-
-      assert {:ok, ["remote", "add"], %{name: "origin"}, []} =
-               HelpfulOptions.parse_commands(["remote", "add", "--name", "origin"], definitions)
-    end
-
-    test "matches root command with empty commands list" do
-      definitions = [
-        %{commands: ["sub"], switches: nil, other: nil},
-        %{commands: [], switches: [verbose: %{type: :boolean}], other: :any}
-      ]
-
-      assert {:ok, [], %{verbose: true}, ["file"]} =
-               HelpfulOptions.parse_commands(["--verbose", "file"], definitions)
-    end
-
-    test "returns unknown_command when no definition matches" do
-      definitions = [
-        %{commands: ["remote"], switches: nil, other: nil}
-      ]
-
-      assert {:error, {:unknown_command, ["branch"]}} =
-               HelpfulOptions.parse_commands(["branch", "--verbose"], definitions)
-    end
-
     test "returns unknown_command with empty subcommands when no root definition exists" do
       definitions = [
         %{commands: ["remote"], switches: nil, other: nil}
@@ -38,25 +9,6 @@ defmodule HelpfulOptions.ParseCommandsTest do
 
       assert {:error, {:unknown_command, []}} =
                HelpfulOptions.parse_commands(["--verbose"], definitions)
-    end
-
-    test "returns duplicate_commands error when definitions share the same commands" do
-      definitions = [
-        %{commands: ["remote"], switches: nil, other: nil},
-        %{commands: ["remote"], switches: [verbose: %{type: :boolean}], other: nil}
-      ]
-
-      assert {:error, {:duplicate_commands, ["remote"]}} =
-               HelpfulOptions.parse_commands(["remote"], definitions)
-    end
-
-    test "propagates parse errors from parse/2" do
-      definitions = [
-        %{commands: ["run"], switches: [count: %{type: :integer}], other: nil}
-      ]
-
-      assert {:error, %HelpfulOptions.Errors{switches: %HelpfulOptions.SwitchErrors{incorrect: [{"--count", "abc"}]}}} =
-               HelpfulOptions.parse_commands(["run", "--count", "abc"], definitions)
     end
 
     test "handles other arguments correctly" do
@@ -88,36 +40,6 @@ defmodule HelpfulOptions.ParseCommandsTest do
   end
 
   describe "parse_commands!/2" do
-    test "returns tuple on success" do
-      definitions = [
-        %{commands: ["run"], switches: [verbose: %{type: :boolean}], other: nil}
-      ]
-
-      assert {["run"], %{verbose: true}, []} =
-               HelpfulOptions.parse_commands!(["run", "--verbose"], definitions)
-    end
-
-    test "raises ArgumentError on unknown command" do
-      definitions = [
-        %{commands: ["run"], switches: nil, other: nil}
-      ]
-
-      assert_raise ArgumentError, "unknown command: deploy", fn ->
-        HelpfulOptions.parse_commands!(["deploy"], definitions)
-      end
-    end
-
-    test "raises ArgumentError on duplicate commands" do
-      definitions = [
-        %{commands: ["run"], switches: nil, other: nil},
-        %{commands: ["run"], switches: nil, other: nil}
-      ]
-
-      assert_raise ArgumentError, "duplicate commands: run", fn ->
-        HelpfulOptions.parse_commands!(["run"], definitions)
-      end
-    end
-
     test "raises on parse error" do
       definitions = [
         %{commands: ["run"], switches: [count: %{type: :integer}], other: nil}
