6a586074cfbe18b4de01eb1133ae769c9df39256
Joe Yates <joe.g.yates@gmail.com>
2026-02-28 09:36:02 +0100

    Add README to docs

--- mix.exs
+++ mix.exs
@@ -1,14 +1,17 @@
 defmodule HelpfulOptions.MixProject do
   use Mix.Project
 
+  @version "0.3.3"
+
   def project do
     [
       app: :helpful_options,
-      version: "0.3.3",
+      version: @version,
       elixir: "~> 1.14",
       description: "A configurable command-line otptions parser",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
       package: package()
     ]
   end
@@ -22,6 +25,14 @@ defmodule HelpfulOptions.MixProject do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs() do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 
